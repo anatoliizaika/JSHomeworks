cf83aec278b007ef756988cb06c3e6af138fc433
anatoliizaika <zaikatolya@gmail.com>
2025-03-25 09:51:51 +0200

    added homework 3

---
+++ lesson3/task1.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 10;
+let c = 3;
+
+console.log("Сумма (a + b):", a + b);
+console.log("Різниця (b - c):", b - c);
+console.log("Добуток (a * c):", a * c);
+console.log("Ділення (b / a):", b / a);
+console.log("Залишок від ділення (c % b):", c % b);
+console.log("Піднесення до степеня (a ** b):", a ** b);
---
+++ lesson3/task2.js
@@ -0,0 +1,5 @@
+let age = Math.floor(Math.random() * 21) + 10; 
+let isAdult = age >= 18;
+
+console.log("Вік:", age);
+console.log("Чи повнолітній?:", isAdult);
