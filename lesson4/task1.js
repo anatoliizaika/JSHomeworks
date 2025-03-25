1c21f86683f8101d3d42930829bfe8e92fed405a
anatoliizaika <zaikatolya@gmail.com>
2025-03-25 10:09:03 +0200

    homework 4 is added

---
+++ lesson4/task1.js
@@ -0,0 +1,7 @@
+let number = 4; 
+
+if (number % 2 === 0) {
+    console.log(`Число ${number} є парним`);
+} else {
+    console.log(`Число ${number} є непарним`);
+}
---
+++ lesson4/task2.js
@@ -0,0 +1,15 @@
+let hour = Math.floor(Math.random() * 24);
+
+if (hour >= 0 && hour <= 5) {
+    console.log("Good night!");
+} else if (hour >= 6 && hour <= 11) {
+    console.log("Good morning!");
+} else if (hour >= 12 && hour <= 17) {
+    console.log("Good day!");
+} else if (hour >= 18 && hour <= 24) {
+    console.log("Good evening!");
+} else {
+    console.log("Invalid hour value");
+}
+
+console.log("Current hour:", hour);
